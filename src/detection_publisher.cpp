6d5796bd9b2c90aab0ef91c0274f9d5d4b6ba6b7
Yao Hwang <yaohwang99@gmail.com>
2022-03-09 23:24:17 +0800

    depth image array index error fixed

--- src/detection_publisher.cpp
+++ src/detection_publisher.cpp
@@ -13,39 +13,31 @@
 #include <pthread.h>
 
 #define FOLLOW_DISTANCE 2000
+#define FOLLOW_TOLERANCE 300
+#define FOLLOW_MAX_DISTANCE 2000
+#define FOLLOW_SPEED 5.0/4000;
+
 #define ROTATION_TOLERANCE 20
+#define ROTATION_SPEED 3.0/320
 
 class detection_publisher{
 	private:
 		ros::NodeHandle nh;
 		image_transport::ImageTransport it;
-		image_transport::Publisher pub;
-		image_transport::Subscriber rgb_sub, depth_sub;
+		image_transport::Subscriber depth_sub;
 		ros::Subscriber box_sub;
 		ros::Publisher twist_pub;
-		cv_bridge::CvImagePtr rgb_ptr, depth_ptr;
+		cv_bridge::CvImagePtr depth_ptr;
 		geometry_msgs::Twist twist;
-
-		//Bounding box information
-		cv::Rect2d bbox;
-		
-		//rotation parameters
-		float turn_speed;
-
-		int curr_depth;
 
 		pthread_mutex_t mutex;
 		
 	public:
-		detection_publisher(): it(nh), turn_speed(3.0/320.0), curr_depth(0)
+		detection_publisher(): it(nh)
 		{
 			//Subscribe to topic
-			rgb_sub = it.subscribe("/camera/color/image_raw", 1,&detection_publisher::imageCallback, this);
 			depth_sub = it.subscribe("/camera/aligned_depth_to_color/image_raw", 1,&detection_publisher::depthCallback, this);
 			box_sub = nh.subscribe("/darknet_ros/bounding_boxes", 1,&detection_publisher::boxCallback, this);
-			
-			//New topic which is the result of image.
-			pub = it.advertise("custom_detection_image", 1);
 			
 			//Comtrol command
 			twist_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
@@ -67,98 +59,63 @@ class detection_publisher{
 			twist_pub.publish(twist);
 		}
 		void boxCallback(const darknet_ros_msgs::BoundingBoxes& msg2){
-			//ROS_INFO("boxCallback");
-			pthread_mutex_lock(&mutex);
 
+			system("clear");
+			std::cout << "***\033[1mdarknet_ros_nodes detection_publisher***\n\033[0m";
+
+			// 0 for x, for y
+			int center[2];
+
+			pthread_mutex_lock(&mutex);
 			twist.linear.x = 0;
 			twist.angular.z = 0;
-			bbox.x = 0;
-			bbox.y = 0;
-			bbox.width = 0;
-			bbox.height = 0;
+			pthread_mutex_unlock(&mutex);
 
-			if(rgb_ptr)
+			if(msg2.bounding_boxes.size()!=0) //Yolo detect items.
 			{
-				//Initialize angular velocity is zero
-				
-				
-				if(msg2.bounding_boxes.size()!=0) //Yolo detect items.
+				int i = 0;
+				for(auto iter=msg2.bounding_boxes.begin(); iter!= msg2.bounding_boxes.end(); ++iter, ++i)
 				{
-					for(auto iter=msg2.bounding_boxes.begin(); iter!= msg2.bounding_boxes.end(); ++iter)
-					{
-						if(iter->id == 0) //human's id is 0
-						{ 
-							//Get bounding box information
-							bbox.x = iter->xmin;
-							bbox.y = iter->ymin;
-							bbox.width = iter->xmax - iter->xmin;
-							bbox.height = iter->ymax - iter->ymin;
+					if(iter->id == 0) //human's id is 0
+					{ 
+						//Get bounding box information
+						center[0] = (iter->xmax + iter->xmin) / 2;
+						center[1] = (iter->ymax + iter->ymin) / 2;
+
+						//640 x 480 for each image
+						//horizontal distance with center point of image
+						float th = 320 - center[0];
+						if(th < ROTATION_TOLERANCE && th > -ROTATION_TOLERANCE) th = 0;
 							
-							//640 x 480 for each image
-							//horizontal distance with center point of image
-							float th = 320 - (iter->xmax + iter->xmin) / 2.0;
-							if(th < ROTATION_TOLERANCE && th > -ROTATION_TOLERANCE) th = 0;
-							
-							//counting angular velocity
-							twist.angular.z = th * turn_speed;
-							
-							break;
-						}
+						//counting angular velocity
+						pthread_mutex_lock(&mutex);
+						twist.angular.z = th * ROTATION_SPEED;
+						pthread_mutex_unlock(&mutex);
+
+						break;
 					}
 				}
 			}
 
-			if(depth_ptr && bbox.width != 0)
+			std::cout << "bounding box center : ( " << center[0] << " , " << center[1] << " )\n";
+
+			pthread_mutex_lock(&mutex);
+			if(depth_ptr && center[0] != 0 && center[1] != 0)
 			{
-				curr_depth = depth_ptr->image.at<u_int16_t>(bbox.x + bbox.width/2, bbox.y + bbox.height);
-				if(curr_depth < FOLLOW_DISTANCE-300) twist.linear.x = - 0.4;
-				else if(curr_depth > FOLLOW_DISTANCE+300) twist.linear.x = 0.4;
+				int curr_depth = depth_ptr->image.at<uint16_t>(center[1], center[0]);
+				std::cout << "\033[0;33mcenter depth : " << curr_depth  << "\n\033[0m";
+				curr_depth -= FOLLOW_DISTANCE;
+				if(curr_depth < FOLLOW_TOLERANCE && curr_depth > -FOLLOW_TOLERANCE) curr_depth = 0;
+				else if(curr_depth > FOLLOW_MAX_DISTANCE) curr_depth = 0;
+				else if(curr_depth < 0) curr_depth = 0;
+				twist.linear.x = curr_depth * FOLLOW_SPEED;
 			}
-			
+			else
+			{
+				std::cout << "\033[0;31m[ERROR] Align Depth\033[0m\n";;
+			}
+				
 			twist_pub.publish(twist);
-			pthread_mutex_unlock(&mutex);
-			show();
-		}
-  	
-		void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-		{
-			//ROS_INFO("imageCallback");
-			pthread_mutex_lock(&mutex);
-			try
-			{
-				rgb_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-				cv::imshow("rgb image", rgb_ptr->image);
-			}
-			catch (cv_bridge::Exception& e)
-			{
-				ROS_ERROR("cv_bridge exception: %s", e.what());
-			}
-			pthread_mutex_unlock(&mutex);
-		}
-
-		void depthCallback(const sensor_msgs::ImageConstPtr& msg)
-		{
-			pthread_mutex_lock(&mutex);
-			try
-			{
-				depth_ptr = cv_bridge::toCvCopy(msg);
-			}
-			catch (cv_bridge::Exception& e)
-			{
-				ROS_ERROR("cv_bridge exception: %s", e.what());
-			}
-			pthread_mutex_unlock(&mutex);
-		}
-
-		void show()
-		{
-			pthread_mutex_lock(&mutex);
-			system("clear");
-			std::cout << "***\033[1mdarknet_ros_nodes detection_publisher***\n\033[0m";
-			std::cout << ((rgb_ptr) ? "\033[0;32m[OK] RGB Image\033[0m\n" :  "\033[0;31m[ERROR] RGB Image\033[0m\n");
-			std::cout << ((depth_ptr) ? "\033[0;32m[OK] Align Depth\033[0m\n" :  "\033[0;31m[ERROR] Align Depth\033[0m\n");
-			std::cout << "bounding box center : ( " << bbox.x + bbox.width/2.0 << " , " << bbox.y + bbox.height/2.0 << " )\n";
-			std::cout << "\033[0;33mcenter depth : " << curr_depth  << "\n\033[0m";
 			std::cout << "twist :\n"
 					  << "\tlinear  x : " << twist.linear.x << "\n"
 					  << "\tlinear  y : " << twist.linear.y << "\n"
@@ -166,10 +123,36 @@ class detection_publisher{
 					  << "\tangular x : " << twist.angular.x << "\n"
 					  << "\tangular y : " << twist.angular.y << "\n"
 					  << "\tangular z : " << twist.angular.z << "\n";
-			std::cout << "rotation tolerance : " << ROTATION_TOLERANCE << "\n";
-			std::cout << "Follow distance : " << FOLLOW_DISTANCE << "\n";
 			pthread_mutex_unlock(&mutex);
 		}
+
+		void depthCallback(const sensor_msgs::ImageConstPtr& msg)
+		{
+			try
+			{
+				pthread_mutex_lock(&mutex);
+				depth_ptr = cv_bridge::toCvCopy(msg);
+				pthread_mutex_unlock(&mutex);
+			}
+			catch (cv_bridge::Exception& e)
+			{
+				ROS_ERROR("cv_bridge exception: %s", e.what());
+			}
+		}
+
+		// void show()
+		// {
+		// 	pthread_mutex_lock(&mutex);
+		// 	system("clear");
+		// 	std::cout << "***\033[1mdarknet_ros_nodes detection_publisher***\n\033[0m";
+		// 	std::cout << ((depth_ptr) ? "\033[0;32m[OK] Align Depth\033[0m\n" :  "\033[0;31m[ERROR] Align Depth\033[0m\n");
+		// 	std::cout << "bounding box center : ( " << bbox.x + bbox.width/2 << " , " << bbox.y + bbox.height/2 << " )\n";
+		// 	std::cout << "\033[0;33mcenter depth : " << curr_depth  << "\n\033[0m";
+
+		// 	std::cout << "rotation tolerance : " << ROTATION_TOLERANCE << "\n";
+		// 	std::cout << "Follow distance : " << FOLLOW_DISTANCE << "\n";
+		// 	pthread_mutex_unlock(&mutex);
+		// }
 };
 
 void SIGINT_Handler(int signum)
@@ -179,10 +162,6 @@ void SIGINT_Handler(int signum)
 
 int main(int argc, char** argv)
 {
-	//cv::namedWindow("depth image");
-  	//cv::startWindowThread();
-	cv::namedWindow("rgb image");
-  	cv::startWindowThread();
 	signal(SIGINT, SIGINT_Handler);  
 	ros::init(argc, argv, "detection_publisher");
 	detection_publisher ic;
